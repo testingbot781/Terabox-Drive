6d1167f0eaed3f1dd9d1b569a181e64bed63eae9
iamrita-ai <amritanoob2005@gmail.com>
2026-01-02 12:04:16 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,2 @@
-from handlers.start import *
-from handlers.help import *
-from handlers.settings import *
-from handlers.broadcast import *
-from handlers.premium import *
-from handlers.cancel import *
-from handlers.link_handler import *
-from handlers.file_handler import *
+# This file must be empty for Pyrogram plugins to work correctly
+# Do not add any imports here
