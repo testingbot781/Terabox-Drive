107fe3af84a35531a294cc113dd44253bab94b3c
iamrita-ai <amritanoob2005@gmail.com>
2026-01-02 08:57:33 +0530

    Update init.py

--- database/init.py
+++ database/init.py
@@ -1,5 +1,8 @@
 from database.mongodb import Database
 from database.users import UserDatabase
 
+# Create global instances
 db = Database()
 user_db = UserDatabase()
+
+__all__ = ['db', 'user_db', 'Database', 'UserDatabase']
